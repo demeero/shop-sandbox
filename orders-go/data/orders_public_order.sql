694b85a79d4e76daad86f1b2a9882eb99e4f7120
Danylo Miroshnichenko <miroshnik.dan@gmail.com>
2020-12-23 00:20:06 +0200

    orders: Add created_at column (#11)

--- orders-go/data/orders_public_order.sql
+++ orders-go/data/orders_public_order.sql
@@ -1,24 +1,25 @@
 create table "order"
 (
-    id              serial            not null
+    id              serial                              not null
         constraint order_pkey
             primary key,
-    user_id         text              not null,
-    total_units     integer default 0 not null,
-    total_nanos     integer default 0 not null,
-    contact_name    text              not null,
-    phone           text              not null,
-    city            text              not null,
-    address1        text              not null,
+    user_id         text                                not null,
+    total_units     integer   default 0                 not null,
+    total_nanos     integer   default 0                 not null,
+    contact_name    text                                not null,
+    phone           text                                not null,
+    city            text                                not null,
+    address1        text                                not null,
     address2        text,
-    order_status_id integer           not null
+    order_status_id integer                             not null
         constraint order_order_status_id_fk
             references order_status
-            on update cascade on delete cascade
+            on update cascade on delete cascade,
+    created_at      timestamp default CURRENT_TIMESTAMP not null
 );
 
 alter table "order"
     owner to postgres;
 
-INSERT INTO public."order" (id, user_id, total_units, total_nanos, contact_name, phone, city, address1, address2, order_status_id) VALUES (2, '2', 200, 40, 'Contact Name 2', '+380687333231', 'Odessa', 'NP #40', null, 2);
-INSERT INTO public."order" (id, user_id, total_units, total_nanos, contact_name, phone, city, address1, address2, order_status_id) VALUES (1, '1', 1, 0, 'Contact Name 1', '+380687333230', 'Odessa', 'NP #40', null, 1);
+INSERT INTO public."order" (id, user_id, total_units, total_nanos, contact_name, phone, city, address1, address2, order_status_id, created_at) VALUES (2, '2', 200, 40, 'Contact Name 2', '+380687333231', 'Odessa', 'NP #40', null, 2, '2020-12-22 22:04:45.489884');
+INSERT INTO public."order" (id, user_id, total_units, total_nanos, contact_name, phone, city, address1, address2, order_status_id, created_at) VALUES (1, '1', 1, 0, 'Contact Name 1', '+380687333230', 'Odessa', 'NP #40', null, 1, '2020-12-22 22:04:45.489884');
--- orders-go/go.mod
+++ orders-go/go.mod
@@ -4,7 +4,10 @@ go 1.15
 
 require (
 	github.com/demeero/shop-sandbox/proto/gen/go v0.0.0-20201222214453-8064f28dc67c
+	github.com/golang/protobuf v1.4.3
 	github.com/jackc/pgx/v4 v4.10.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	google.golang.org/grpc v1.34.0
 )
+
+replace github.com/demeero/shop-sandbox/proto/gen/go => ../proto/gen/go/
